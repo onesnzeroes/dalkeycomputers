f32a3051cd219a91d2a2d8da31d5a17c1992e1a4
martco <martco@pm.me>
2025-04-14 20:47:26 +0100

    Updated homepage with product list and image

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,9 @@
 module.exports = function(eleventyConfig) {
+	  // Add a collection for laptop listings
+  eleventyConfig.addCollection("laptops", function (collectionApi) {
+    return collectionApi.getFilteredByGlob("src/laptops/*.md");
+  });
+
   return {
     dir: {
       input: 'src',
